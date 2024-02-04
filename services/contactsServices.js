757ca44db9a6d9cd816080a0558b19d7c6faf3cc
AnastasiiaKalashnikova <nastasyakalashnikova@icloud.com>
2024-02-04 13:16:43 +0100

    done

---
+++ services/contactsServices.js
@@ -0,0 +1,60 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "../db/contacts.json");
+
+async function listContacts() {
+  const contacts = await fs.readFile(contactsPath, "utf8");
+  return JSON.parse(contacts);
+}
+
+async function getContactById(contactId) {
+  const allContacts = await listContacts();
+  const contact = allContacts.find((item) => item.id === contactId);
+  return contact || null;
+}
+
+async function removeContact(contactId) {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = allContacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return result;
+}
+
+async function addContact({ name, email, phone }) {
+  const allContacts = await listContacts();
+
+  const newContact = {
+    id: nanoid(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  allContacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return newContact;
+}
+
+async function updateContact(contactId, data) {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  allContacts[index] = { ...allContacts[index], ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return allContacts[index];
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
